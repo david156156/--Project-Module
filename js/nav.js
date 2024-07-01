dfb10e6eb0fe4ba9cb76a561ff704274fe3121e9
david shemesh <dyhs1991@gmail.com>
2024-07-01 17:19:09 +0300

    sus

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,8 @@
 function hamburger() {
     let menu = document.getElementById("menui");
     if (menu.style.display == "none") {
-        menu.style.display = "block";
+        menu.style.display = "block"
     } else {
-        menu.style.display = "none";
+        menu.style.display = "none"
     }
 }
