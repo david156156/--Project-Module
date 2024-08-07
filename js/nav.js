7baa4b67270afa0657a5a2e89a0cc1a64256ffbb
david156156 <dyhs1991@gmail.com>
2024-08-07 18:31:26 +0300

    module

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,8 @@
-window.hamburger = function hamburger() {
+function hamburger() {
   let menu = document.getElementById("menui");
   if (menu.style.display == "none") {
     menu.style.display = "block";
   } else {
     menu.style.display = "none";
   }
-};
+}
