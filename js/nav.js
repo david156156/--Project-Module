4431a751cee0e9e71881d4e51cfa571773484f70
david shemesh <dyhs1991@gmail.com>
2024-08-21 21:27:29 +0300

    תיקוני עיצוב

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,13 @@
 function hamburger() {
   let menu = document.getElementById("menui");
+  let hamburgerHover = document.getElementById("hamburger");
   if (menu.style.display == "none") {
     menu.style.display = "block";
+    hamburgerHover.style.color = "#ffffff";
+    hamburgerHover.style.backgroundColor = "#3b3b3b";
   } else {
     menu.style.display = "none";
+    hamburgerHover.style.color = "#1ae0ec";
+    hamburgerHover.style.backgroundColor = "#f0f0f0";
   }
 }
